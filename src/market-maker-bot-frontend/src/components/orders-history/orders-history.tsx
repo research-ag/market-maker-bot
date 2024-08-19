e74403db3b9d404df0e64863bce20f271b85cb9a
Dmitry Panchenko <dmitriy.panchenko@gmail.com>
2024-08-19 05:47:27 +0300

    chore: refactoring and fixing frontend

--- src/market-maker-bot-frontend/src/components/orders-history/orders-history.tsx
+++ src/market-maker-bot-frontend/src/components/orders-history/orders-history.tsx
@@ -1,26 +1,64 @@
 import { Box, Table } from '@mui/joy';
 
-import { useOrdersHistory } from '../../integration';
+import { useGetHistory } from '../../integration';
 
 export const OrdersHistory = () => {
-  const { data: history, isFetching } = useOrdersHistory();
+  const { data: history, isFetching } = useGetHistory();
   return (
     <Box sx={{ width: '100%', overflow: 'auto' }}>
       <Table>
+        <colgroup>
+          <col style={{ width: '50px' }} />
+          <col style={{ width: '50px' }} />
+          <col style={{ width: '50px' }} />
+          <col style={{ width: '200px' }} />
+          <col style={{ width: '200px' }} />
+        </colgroup>
+        <thead>
+        <tr>
+          <th>Base Token</th>
+          <th>Quote Token</th>
+          <th>Spread</th>
+          <th>BID</th>
+          <th>ASK</th>
+        </tr>
+        </thead>
         <tbody>
         {isFetching && (
           <tr>
-            <td>
+            <td colSpan={6}>
               Loading...
             </td>
           </tr>
         )}
-        {isFetching && (history ?? []).map((item: any, i: number) => {
+        {!isFetching && (history ?? []).map((item: any, i: number) => {
           return (
             <tr key={i}>
               <td>
-                {item}
+                {item?.pair?.base?.symbol}
               </td>
+              <td>
+                {item?.pair?.quote?.symbol}
+              </td>
+              <td>
+                {item?.pair?.spread_value}
+              </td>
+              {item.message === 'OK' ? (
+                <>
+                  <td>
+                    {item?.bidOrder?.amount?.toString()} as {item?.bidOrder?.price}
+                  </td>
+                  <td>
+                    {item?.askOrder?.amount?.toString()} as {item?.askOrder?.price}
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td colSpan={2}>
+                    {item?.message}
+                  </td>
+                </>
+              )}
             </tr>
           );
         })}
