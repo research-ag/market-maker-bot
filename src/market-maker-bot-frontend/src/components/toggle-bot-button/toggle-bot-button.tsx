b4ada2e6c1f0f16052639dfe748142a5c0ad42cc
Dmitriy Panchenko <dmitriy.panchenko@gmail.com>
2024-08-30 18:04:24 +0300

    chore: UI improvements for start/stop button behavior

--- src/market-maker-bot-frontend/src/components/toggle-bot-button/toggle-bot-button.tsx
+++ src/market-maker-bot-frontend/src/components/toggle-bot-button/toggle-bot-button.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import { Button } from '@mui/joy';
 
 import { useStartBot, useStopBot } from '../../integration';
+import { ErrorAlert}  from '../error-alert';
 import { SetTimerModal } from './set-timer-modal';
 
 export const ToggleBotButton = (props : { isRunning: boolean, isFetching: boolean, currentTimer: BigInt }) => {
+  const { mutate: startBot, error, isLoading: isStartLoading } = useStartBot();
   const { mutate: stopBot, isLoading: isStopLoading } = useStopBot();
-  const { isLoading: isStartLoading } = useStartBot();
 
   const [isStartModalOpen, setIsStartModalOpen] = useState(false);
   const openStartModal = () => setIsStartModalOpen(true);
@@ -23,12 +24,18 @@ export const ToggleBotButton = (props : { isRunning: boolean, isFetching: boolea
     }
   };
 
+  const handleSubmit = (timer: bigint) => {
+    startBot(timer);
+    closeStartModal();
+  };
+
   return (
     <>
+      {!!error && <ErrorAlert errorMessage={(error as Error).message} />}
       <Button onClick={handleClick} color={isLoading ? 'warning' : (!isStarted ? 'success' : 'danger')}>
-        {isLoading ? 'Loading...' : (!isStarted ? 'Start' : 'Stop')}
+        {isLoading ? (!isStarted ? 'Starting...' : 'Stopping...') : (!isStarted ? 'Start' : 'Stop')}
       </Button>
-      <SetTimerModal isOpen={isStartModalOpen} onClose={closeStartModal} currentTimer={props.currentTimer} />
+      <SetTimerModal isOpen={isStartModalOpen} onClose={closeStartModal} currentTimer={props.currentTimer} onSubmit={handleSubmit}/>
     </>
   );
 };
