e6d2676b5154e1edbeca0e2dae70db552d6169fb
Dmitriy Panchenko <dmitriy.panchenko@gmail.com>
2024-08-30 16:38:48 +0300

    chore: refactor start stop button with set timer value

--- src/market-maker-bot-frontend/src/components/toggle-bot-button/toggle-bot-button.tsx
+++ src/market-maker-bot-frontend/src/components/toggle-bot-button/toggle-bot-button.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { Button } from '@mui/joy';
 
 import { useStartBot, useStopBot } from '../../integration';
+import { SetTimerModal } from './set-timer-modal';
 
-export const ToggleBotButton = (props : { isRunning: boolean, isFetching: boolean }) => {
-  const { mutate: startBot, isLoading: isStartLoading } = useStartBot();
+export const ToggleBotButton = (props : { isRunning: boolean, isFetching: boolean, currentTimer: BigInt }) => {
   const { mutate: stopBot, isLoading: isStopLoading } = useStopBot();
+  const { isLoading: isStartLoading } = useStartBot();
+
+  const [isStartModalOpen, setIsStartModalOpen] = useState(false);
+  const openStartModal = () => setIsStartModalOpen(true);
+  const closeStartModal = () => setIsStartModalOpen(false);
 
   const isStarted = !!props?.isRunning;
   const isLoading = !!props?.isFetching || isStartLoading || isStopLoading;
@@ -13,13 +19,16 @@ export const ToggleBotButton = (props : { isRunning: boolean, isFetching: boolea
     if (isStarted) {
       stopBot();
     } else {
-      startBot();
+      openStartModal();
     }
   };
 
   return (
-    <Button onClick={handleClick} color={isLoading ? 'warning' : (!isStarted ? 'success' : 'danger')}>
-      {isLoading ? 'Loading...' : (!isStarted ? 'Start' : 'Stop')}
-    </Button>
+    <>
+      <Button onClick={handleClick} color={isLoading ? 'warning' : (!isStarted ? 'success' : 'danger')}>
+        {isLoading ? 'Loading...' : (!isStarted ? 'Start' : 'Stop')}
+      </Button>
+      <SetTimerModal isOpen={isStartModalOpen} onClose={closeStartModal} currentTimer={props.currentTimer} />
+    </>
   );
 };
