819f088af0bdd75c2276bcf27a77f778ed0ad6bd
Andy <andy.gura.jd@gmail.com>
2024-10-24 12:52:36 +0300

    fixed frontend app crash

--- src/market-maker-bot-frontend/src/components/pairs/pairs-table/pairs-table.tsx
+++ src/market-maker-bot-frontend/src/components/pairs/pairs-table/pairs-table.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Table } from '@mui/joy';
+import {Box, Table} from '@mui/joy';
 
-import { useGetPairsList } from '../../../integration';
+import {useGetPairsList, useGetQuoteInfo} from '../../../integration';
 import InfoItem from '../../root/info-item';
-import { displayWithDecimals } from '../../../utils';
 
 export const PairsTable = () => {
+  const {data: quoteInfo, isFetching: isQuoteInfoFetching} = useGetQuoteInfo();
   const { data: pairsList, isFetching } = useGetPairsList();
   return (
     <Box sx={{ width: '100%', overflow: 'auto' }}>
@@ -22,25 +22,25 @@ export const PairsTable = () => {
         </tr>
         </thead>
         <tbody>
-        {(isFetching) && (
+        {(isFetching || isQuoteInfoFetching) && (
           <tr>
             <td colSpan={3}>
               Loading...
             </td>
             </tr>
         )}
-        {!(isFetching) && (pairsList ?? []).map((pair: any, i: number) => {
+        {!(isFetching || isQuoteInfoFetching) && (pairsList ?? []).map((pair, i) => {
           return (
             <tr key={i}>
               <td>
-                <InfoItem content={pair.base_symbol} withCopy={true} />
-                <InfoItem content={pair.base_principal.toText()} withCopy={true} />
-                <InfoItem content={`Decimals ${pair.base_decimals}`} />
+                <InfoItem content={pair.base.symbol} withCopy={true}/>
+                <InfoItem content={pair.base.principal.toText()} withCopy={true}/>
+                <InfoItem content={`Decimals ${pair.base.decimals}`}/>
               </td>
               <td>
-                <InfoItem content={pair.quote_symbol} withCopy={true} />
-                <InfoItem content={pair.quote_principal.toText()} withCopy={true} />
-                <InfoItem content={`Decimals ${pair.quote_decimals}`} />
+                <InfoItem content={quoteInfo?.symbol || '-'} withCopy={true}/>
+                <InfoItem content={quoteInfo?.principal.toText() || '-'} withCopy={true}/>
+                <InfoItem content={`Decimals ${quoteInfo?.decimals || '-'}`}/>
               </td>
               <td>{pair.spread_value}</td>
             </tr>
